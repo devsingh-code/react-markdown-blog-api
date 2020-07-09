6c194eeec7a1f9029932ffee0e40d36427cb9fa6
Deovrat Singh <devsingh6n@gmail.com>
2020-07-09 23:22:17 +0530

    Static files settings added

--- blog/settings.py
+++ blog/settings.py
@@ -133,6 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATIC_ROOT = os.path.join(BASE_DIR,"static_root")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media_root")
+MEDIA_URL ='/media/'
+
 
 #added for rest framework
 REST_FRAMEWORK = {
